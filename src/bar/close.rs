97b4b1fb0853a786787f8a8ec137d877fefa4973
Abhishek Sane <abhishekr.sane@gmail.com>
2020-09-15 15:50:06 -0400

    WIP: Register the appbar with Windows Application Desktop Toolbars API (#159)
    Fixes #116 
    
    * Added function call to close appbars if they exist before Nog quits
    
    * Register the appbar with Windows and set its location
    
    * Send message to unregister appbar when appbar is closed
    
    * fixes
    
    Co-authored-by: TimUntersberger <timuntersberger2@gmail.com>

--- src/bar/close.rs
+++ src/bar/close.rs
@@ -1,6 +1,8 @@
 use super::{get_windows, FONT};
 use log::{debug, info};
 use std::ffi::CString;
+use winapi::shared::windef::HWND;
+use winapi::um::shellapi::{SHAppBarMessage, ABM_REMOVE, APPBARDATA};
 use winapi::um::winuser::UnregisterClassA;
 
 pub fn close() {
@@ -8,6 +10,14 @@ pub fn close() {
         info!("Closing appbar");
 
         for window in get_windows() {
+            let mut appbar_data: APPBARDATA = APPBARDATA {
+                cbSize: 4 + 4 + 4 + 4 + 16 + 4,
+                hWnd: window.id as HWND,
+                ..Default::default()
+            };
+
+            SHAppBarMessage(ABM_REMOVE, &mut appbar_data as *mut APPBARDATA);
+
             window.close();
         }
 
--- src/bar/visibility.rs
+++ src/bar/visibility.rs
@@ -3,13 +3,17 @@ use super::{get_windows, redraw::redraw};
 #[allow(dead_code)]
 pub fn hide() {
     for window in get_windows() {
-        window.hide();
+        if !window.is_hidden() {
+            window.hide();
+        }
     }
 }
 
 pub fn show() {
     for window in get_windows() {
-        window.show();
+        if window.is_hidden() {
+            window.show();
+        }
     }
 
     redraw();
