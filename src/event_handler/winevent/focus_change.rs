64971e4227bb8ff2a88a46c61301f2fb04acda87
TimUntersberger <timuntersberger2@gmail.com>
2020-10-24 10:22:09 +0200

    fix: correctly manual focus change

--- src/event_handler/winevent/focus_change.rs
+++ src/event_handler/winevent/focus_change.rs
@@ -2,8 +2,10 @@ use crate::{system::NativeWindow, system::SystemResult, AppState};
 
 pub fn handle(state: &mut AppState, window: NativeWindow) -> SystemResult {
     if let Some((g, _)) = state.find_window(window.id) {
+        println!("found");
         g.focus_stack.clear();
         g.focused_window_id = Some(window.id);
+        state.workspace_id = g.id;
     }
 
     Ok(())
