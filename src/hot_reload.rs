7814c7a694a9915378755f75355b4d7ea7142b34
mramirez <ramirezmike2@gmail.com>
2020-11-29 09:53:02 -0500

    Fix: #189 keybindings were being ignored after config changes

--- src/hot_reload.rs
+++ src/hot_reload.rs
@@ -14,7 +14,8 @@ pub fn update_config(state_arc: Arc<Mutex<AppState>>, new_config: Config) -> Sys
     state.keybindings_manager.stop();
 
     state.config = new_config;
-    state.keybindings_manager = KbManager::new(state.config.keybindings.clone());
+    let new_keybindings = state.config.keybindings.clone();
+    state.keybindings_manager.update_bindings(new_keybindings);
 
     if work_mode {
         if old_config.remove_task_bar && !state.config.remove_task_bar {
