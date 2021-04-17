d3fbf4e955c59bd4f3a0944cd191ae105bb2ee40
TimUntersberger <timuntersberger2@gmail.com>
2021-04-17 23:56:20 +0200

    chore: remove nogscript related source code and rust side component implementations

--- twm/src/bar/component/padding.rs
+++
@@ -1,9 +0,0 @@
-use super::{Component, ComponentText};
-
-pub fn create(amount: i32) -> Component {
-    Component::new("Padding", move |_| {
-        Ok(vec![ComponentText::new().with_display_text(
-            " ".to_string().repeat(amount as usize),
-        )])
-    })
-}
