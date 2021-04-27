e376445c5a535ed08cdfc120f5f53b4ad372b001
TimUntersberger <timuntersberger2@gmail.com>
2021-04-27 17:10:31 +0200

    update download_release.ps1

--- bin/download_release.ps1
+++ bin/download_release.ps1
@@ -1,10 +1,16 @@
-$asset_url = "https://github.com/neovim/neovim/releases/download/v0.4.4/nvim-win64.zip"
+param(
+  [Parameter(Mandatory=$True, Position=0)]
+  [string]
+  $Release
+)
+
+$asset_url = "https://github.com/TimUntersberger/nog/releases/download/$Release/Nog.zip"
 $asset_name = "__temp_release_asset"
 $out_path = "$env:APPDATA/nog_lua"
 
 invoke-webrequest $asset_url -outfile "$asset_name.zip"
 expand-archive "./$asset_name.zip" "$asset_name"
-move-item "$asset_name/Neovim" $out_path
+move-item "$asset_name/Nog" $out_path
 
 remove-item "./$asset_name.zip"
 remove-item "./$asset_name"
