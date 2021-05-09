416a8e2ba9f8986bd531281b5e15e9fbbc67da25
TimUntersberger <timuntersberger2@gmail.com>
2021-05-09 15:43:22 +0200

    whoops

--- bin/download_release.ps1
+++ bin/download_release.ps1
@@ -6,7 +6,7 @@ param(
 
 $asset_url = "https://github.com/TimUntersberger/nog/releases/download/$Release/Nog.zip"
 $asset_name = "__temp_release_asset"
-$out_path = "$env:APPDATA/nog_lua"
+$out_path = "$env:APPDATA/nog"
 
 invoke-webrequest $asset_url -outfile "$asset_name.zip"
 expand-archive "./$asset_name.zip" "$asset_name"
