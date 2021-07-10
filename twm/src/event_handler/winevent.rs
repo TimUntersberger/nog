6de32d248b393704dc53ffdcf387dec233de4fd0
TimUntersberger <timuntersberger2@gmail.com>
2021-07-10 22:57:22 +0200

    change(#288): minimizing window -> gets unmanaged

--- twm/src/event_handler/winevent.rs
+++ twm/src/event_handler/winevent.rs
@@ -45,7 +45,17 @@ pub fn handle(state: &mut AppState, ev: WinEvent) -> SystemResult {
         WinEventType::Destroy => destroy::handle(state, ev.window, grid_id)?,
         WinEventType::Show(ignore) => show::handle(state, ev.window, ignore)?,
         WinEventType::FocusChange => focus_change::handle(state, ev.window)?,
-        WinEventType::Hide => {}
+        WinEventType::Minimize => {
+            if let Some(mut win) = state
+                .find_grid_containing_window(ev.window.id)
+                .and_then(|g| g.remove_by_window_id(ev.window.id))
+            {
+                win.cleanup()?;
+                state.get_current_display().refresh_grid(&state.config)?;
+            }
+        },
+        WinEventType::Hide
+        | WinEventType::Unminimize => {}
     };
 
     Ok(())
